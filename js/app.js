b3c2aed2579e1aca9b759710f802106651567fe7
haris wedira <hariswediraa@gmail.com>
2019-08-06 13:53:08 +0700

    Adding feature add new chat

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
 // dom query
 const chatlist = document.querySelector('.chat-list');
+const newChatForm = document.querySelector('.new-chat');
+
+newChatForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const message = newChatForm.message.value.trim();
+    chatroom.addChat(message)
+        .then(() => newChatForm.reset())
+        .catch(err => console.log(err));
+});
 
 // class instance
 const chatUI = new ChatUI(chatlist);
