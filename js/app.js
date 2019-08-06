7683cf8be014e0c30813c80b2dc335fe3a4ce614
haris wedira <hariswediraa@gmail.com>
2019-08-06 14:15:05 +0700

    Updating username

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 // dom query
 const chatlist = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updtmssg = document.querySelector('.update-message');
 
 newChatForm.addEventListener('submit', e => {
     e.preventDefault();
@@ -10,9 +12,22 @@ newChatForm.addEventListener('submit', e => {
         .catch(err => console.log(err));
 });
 
+newNameForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const username = newNameForm.name.value.trim();
+    chatroom.updateName(username);
+    newNameForm.reset();
+    updtmssg.innerText = `Your name ${username}`;
+    setTimeout(() => {
+        updtmssg.innerText = '';
+    }, 3000)
+})
+
+const username = localStorage.username ? localStorage.username : 'anon';
+
 // class instance
 const chatUI = new ChatUI(chatlist);
-const chatroom = new Chatroom('general', 'jhon');
+const chatroom = new Chatroom('general', username);
 
 // get chat and render
 chatroom.getChats(data => chatUI.render(data));
